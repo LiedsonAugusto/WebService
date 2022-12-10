d8a2e405f1ef21affe0de7b06e8ca0cda0e46a3b
Liedson <liedsonaugusto@gmail.com>
2022-12-10 16:24:42 -0300

    Entidade User e UserResource

---
+++ src/main/java/com/springproject/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.springproject.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springproject.curso.entities.User;
+
+
+@RestController // recurso web implementada por um controlador rest
+@RequestMapping(value="/users") //caminho localhost:8080/users
+public class UserResource {
+
+	@GetMapping // método que responde a requisição get do http
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "aaaa", "aaa@gmail.com", "9999", "12345");
+		return ResponseEntity.ok().body(u); // retorna User feita a requisição
+	}
+	
+}
