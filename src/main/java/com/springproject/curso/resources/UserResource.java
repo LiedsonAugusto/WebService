9e3fd85363afb08e848987fff744fecd0a3d1b3d
Liedson <liedsonaugusto@gmail.com>
2022-12-28 10:24:33 -0300

    UserService e Order

--- src/main/java/com/springproject/curso/resources/UserResource.java
+++ src/main/java/com/springproject/curso/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.springproject.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springproject.curso.entities.User;
+import com.springproject.curso.services.UserService;
 
 
 @RestController //recurso web implementada por um controlador rest
 @RequestMapping(value="/users") // setando caminho para localhost:8080/users
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping // método que responde a requisição get do http
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "aaaa", "aaa@gmail.com", "9999", "12345");
-		return ResponseEntity.ok().body(u); // retorna User feita a requisição
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list); // retorna User feita a requisição
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
