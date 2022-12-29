778179f58f72f8dbc196d2836e223c5f674145b0
Liedson <liedsonaugusto@gmail.com>
2022-12-29 17:20:14 -0300

    Entidade Category

---
+++ src/main/java/com/springproject/curso/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.springproject.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.curso.entities.Category;
+import com.springproject.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+}
