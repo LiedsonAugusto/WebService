280452668b94c986f422fa16b034554956c81db7
Liedson <liedsonaugusto@gmail.com>
2022-12-30 09:44:34 -0300

    Service e Resource do Products

---
+++ src/main/java/com/springproject/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.springproject.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.curso.entities.Product;
+import com.springproject.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> opt = productRepository.findById(id);
+		return opt.get();
+	}
+}
